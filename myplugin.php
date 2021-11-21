26cc057704b0ed9f6d6127792d4932aa8038c1e1
DESKTOP-HIGNV92\Bijay <bijaythakur330@gmail.com>
2021-11-21 16:06:12 +0545

    added an uninstall feature

--- myplugin.php
+++ myplugin.php
@@ -17,6 +17,13 @@ if (!defined('ABSPATH')) {
   exit;
 }
 
+// load text domain
+function myplugin_load_textdomain()
+{
+
+  load_plugin_textdomain('myplugin', false, plugin_dir_path(__FILE__) . 'languages/');
+}
+add_action('plugins_loaded', 'myplugin_load_textdomain');
 
 // if admin area
 if (is_admin()) {
@@ -29,6 +36,10 @@ if (is_admin()) {
   require_once plugin_dir_path(__FILE__) . 'admin/settings-validate.php';
 }
 
+// include dependencies: admin and public
+require_once plugin_dir_path(__FILE__) . 'includes/core-functions.php';
+
+
 
 // default plugin options
 function myplugin_options_default()
@@ -36,11 +47,24 @@ function myplugin_options_default()
 
   return array(
     'custom_url'    =>  'https://wordpress.org',
-    'custom_title'    =>  'Powered by WordPress',
+    'custom_title'    =>  esc_html__('Powered by WordPress', 'myplugin'),
     'custom_style'    =>  'disable',
-    'custom_message'  =>   '<p class="custom-message">My custom message</p>',
-    'custom_footer'   =>  'Special message for users',
+    'custom_message'  =>   '<p class="custom-message">' . esc_html__('My custom message', 'myplugin') . '</p>',
+    'custom_footer'   =>  esc_html__('Special message for users', 'myplugin'),
     'custom_toolbar'    =>   false,
     'custom_scheme'   =>  'default',
   );
 }
+
+/* 
+// remove options on uninstall
+function myplugin_on_uninstall()
+{
+
+  if (!current_user_can('activate_plugins')) return;
+
+  delete_option('myplugin_options');
+}
+register_uninstall_hook(__FILE__, 'myplugin_on_uninstall');
+
+*/
